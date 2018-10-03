bd02d24726a93cd7e45ac6d3d8e9dc69b20fcfda
Corey Sather <manicmachine@users.noreply.github.com>
2018-10-03 11:33:25 -0500

    Uploaded file to incorrect location; relocating.

--- PrioritySort.java
+++
@@ -1,7 +0,0 @@
-class PrioritySort implements Comparator<Event> {
-    // Sort events in ascending order of priority.
-    
-    public int compare(Event a, Event b) {
-        return a.getEventPriority() - b.getEventPriority();
-    }
-}
