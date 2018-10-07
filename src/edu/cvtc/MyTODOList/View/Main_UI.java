0aa4ad1285175d152cf5b2c1b718871bce4733ed
lfriedl28 <lfriedl@student.cvtc.edu>
2018-10-07 14:57:39 -0500

    Add files via upload

--- src/edu/cvtc/MyTODOList/View/Main_UI.java
+++ src/edu/cvtc/MyTODOList/View/Main_UI.java
@@ -11,11 +11,29 @@ import javax.swing.JTable;
 import java.awt.Color;
 import javax.swing.table.DefaultTableModel;
 import java.awt.Button;
+import javax.swing.JSplitPane;
+import java.awt.Panel;
+import javax.swing.BoxLayout;
+import java.awt.FlowLayout;
+import java.awt.Component;
+import javax.swing.JButton;
+import javax.swing.border.BevelBorder;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Canvas;
+import javax.swing.SwingConstants;
+import javax.swing.JSpinner;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.ListSelectionModel;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import java.awt.Font;
+import javax.swing.JTextPane;
 
 public class Main_UI extends JFrame {
 
 	private JPanel contentPane;
-	private JTable Week_Calendar;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -41,26 +59,70 @@ public class Main_UI extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1288, 756);
 		contentPane = new JPanel();
+		contentPane.setForeground(Color.LIGHT_GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(1, 0, 0, 0));
+		contentPane.setLayout(null);
 		
-		Week_Calendar = new JTable();
-		Week_Calendar.setModel(new DefaultTableModel(
+		table = new JTable();
+		table.setBounds(26, 175, 1100, 400);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setBorder(null);
+		table.setAlignmentX(Component.LEFT_ALIGNMENT);
+		table.setAlignmentY(Component.TOP_ALIGNMENT);
+		table.setModel(new DefaultTableModel(
 			new Object[][] {
+				{null, null, null, null, null, null, ""},
+				{null, null, null, null, null, null, ""},
 				{null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null},
-				{"", null, null, null, null, null, null},
 				{null, null, null, null, null, null, null},
 			},
 			new String[] {
-				"Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
+				"New column", "New column", "New column", "New column", "New column", "New column", "Events"
 			}
 		));
-		Week_Calendar.setColumnSelectionAllowed(true);
-		Week_Calendar.setCellSelectionEnabled(true);
-		Week_Calendar.setBackground(Color.LIGHT_GRAY);
-		contentPane.add(Week_Calendar);
+		table.setCellSelectionEnabled(true);
+		table.setBackground(Color.LIGHT_GRAY);
+		table.setRowHeight(100);
+		contentPane.add(table);
+		
+		JPanel ButtonsPanel = new JPanel();
+		ButtonsPanel.setBounds(527, 588, 301, 108);
+		ButtonsPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+		contentPane.add(ButtonsPanel);
+		ButtonsPanel.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+		ButtonsPanel.setLayout(null);
+		
+		JButton CancelBtn = new JButton("Cancel");
+		CancelBtn.setBounds(12, 27, 142, 57);
+		ButtonsPanel.add(CancelBtn);
+		
+		JButton SubmitBtn = new JButton("Submit");
+		SubmitBtn.setBounds(159, 27, 130, 57);
+		ButtonsPanel.add(SubmitBtn);
+		
+		JList EventList = new JList();
+		EventList.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		EventList.setModel(new AbstractListModel() {
+			String[] values = new String[] {"Event 1", "Event 2", "Event 3", "Church", "porn", "Crying", "Online poker"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		EventList.setBounds(1138, 13, 120, 683);
+		contentPane.add(EventList);
+		
+		JTextPane MonthLbl = new JTextPane();
+		MonthLbl.setForeground(Color.MAGENTA);
+		MonthLbl.setBackground(Color.PINK);
+		MonthLbl.setFont(new Font("Tahoma", Font.BOLD, 60));
+		MonthLbl.setEditable(false);
+		MonthLbl.setText("MONTH\r\n");
+		MonthLbl.setBounds(527, 51, 235, 76);
+		
+		contentPane.add(MonthLbl);
 	}
-
 }
